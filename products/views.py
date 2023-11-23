29734eecf0ea66aca90539aa7e4e0b9b7fb37e40
Trollope91 <ljtdev91@gmail.com>
2023-11-23 14:23:31 +0000

    Add product editing and deletion for admins

--- products/views.py
+++ products/views.py
@@ -71,12 +71,21 @@ def product_detail(request, product_id):
 
 def add_product(request):
     """ Adds a product to the store """
-    form = ProductForm()
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.success(request, 'Successfully added product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+    else:
+        form = ProductForm()
+        
     template = 'products/add_product.html'
     context = {
         'form': form,
     }
-
 
     return render(request, template, context)
 
@@ -102,3 +111,11 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    """ Delete product from the store """
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product deleted')
+    return redirect(reverse('products'))
+
