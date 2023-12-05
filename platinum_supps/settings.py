712bb12480473708b304312c76a282fa25434639
Trollope91 <ljtdev91@gmail.com>
2023-12-05 15:25:38 +0000

    Add cache control to settings

--- platinum_supps/settings.py
+++ platinum_supps/settings.py
@@ -173,11 +173,19 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=99999999'
+    }
+
+
     AWS_STORAGE_BUCKET_NAME = 'platinum-supps'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+
 
 #static and media
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
