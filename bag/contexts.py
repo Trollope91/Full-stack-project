b4261720f9e7a4c5deeae7b79967fb9db0450360
Trollope91 <ljtdev91@gmail.com>
2023-12-10 05:40:10 +0000

    Add promo code functionality and front end

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,49 +1,91 @@
 from decimal import Decimal
+
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
-from products.models import Product
+
+from products.models import Product, PromoCodes
+
 
 def bag_contents(request):
-
     bag_items = []
     total = 0
     product_count = 0
-    bag = request.session.get('bag',{})
+    bag = request.session.get("bag", {})
+    discount_code = request.session.get("discount_code", None)
+    # request.session['discount_code'] = ''
+
+    # request.session['total_discount'] = 0
+    request.session["total_discount"] = "0"
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
             product = get_object_or_404(Product, pk=item_id)
             total += item_data * product.price
+            total = apply_discount_to_product(
+                request=request,
+                product=product,
+                total=total,
+                discount_code=discount_code,
+                quantity=item_data,
+            )
             product_count += item_data
-            bag_items.append({
-                'item_id': item_id,
-                'quantity': item_data,
-                'product': product,
-            })
+            bag_items.append(
+                {
+                    "item_id": item_id,
+                    "quantity": item_data,
+                    "product": product,
+                }
+            )
         else:
             product = get_object_or_404(Product, pk=item_id)
 
-            if getattr(product, 'has_weight', False):
-                for weight, quantity, in item_data['items_by_weight'].items():
+            if getattr(product, "has_weight", False):
+                for (
+                    weight,
+                    quantity,
+                ) in item_data["items_by_weight"].items():
                     total += quantity * product.price
                     product_count += quantity
-                    bag_items.append({
-                        'item_id': item_id,
-                        'quantity': quantity,
-                        'product': product,
-                        'weight': weight
-                    })
-            elif getattr(product, 'has_sizes', False):
-                for size, quantity, in item_data['items_by_size'].items():
-                    total += quantity * product.price
-                    product_count += quantity
-                    bag_items.append({
-                        'item_id': item_id,
-                        'quantity': quantity,
-                        'product': product,
-                        'size': size
-                    })
+                    total = apply_discount_to_product(
+                        request=request,
+                        product=product,
+                        total=total,
+                        discount_code=discount_code,
+                        quantity=quantity,
+                    )
 
+                    bag_items.append(
+                        {
+                            "item_id": item_id,
+                            "quantity": quantity,
+                            "product": product,
+                            "weight": weight,
+                        }
+                    )
+            elif getattr(product, "has_sizes", False):
+                for (
+                    size,
+                    quantity,
+                ) in item_data["items_by_size"].items():
+                    total += quantity * product.price
+                    total = apply_discount_to_product(
+                        request=request,
+                        product=product,
+                        total=total,
+                        discount_code=discount_code,
+                        quantity=quantity,
+                    )
+
+                    product_count += quantity
+                    bag_items.append(
+                        {
+                            "item_id": item_id,
+                            "quantity": quantity,
+                            "product": product,
+                            "size": size,
+                        }
+                    )
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -55,14 +97,81 @@ def bag_contents(request):
     grand_total = delivery + total
 
     context = {
-        'bag_items': bag_items,
-        'total': total,
-        'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total,
-
+        "bag_items": bag_items,
+        "total": total,
+        "product_count": product_count,
+        "delivery": delivery,
+        "free_delivery_delta": free_delivery_delta,
+        "free_delivery_threshold": settings.FREE_DELIVERY_THRESHOLD,
+        "grand_total": grand_total,
     }
 
     return context
+
+
+# def apply_discount_to_product(request, product, total, discount_code):
+#     if discount_code:
+#         try:
+#             # Retrieve the discount code from the database
+#             discount = PromoCodes.objects.get(code=discount_code)
+#             # Apply the discount to the total
+#             #discount_percentage = Decimal(discount.discount_percentage) / 100
+
+#             # Retrieve the current total from the session
+#             #total = request.session.get('total', 0)
+
+#             # Calculate the discounted total
+#             discountValue = round(((total / 100) * discount.discount_percentage),2)
+#             total = (total - discountValue)
+#             request.session['total_discount'] = str(request.session.get('total_discount', Decimal('0')) + discountValue)
+
+#             request.session['total_discount'] = str(request.session.get('total_discount', Decimal('0')) + discountValue)
+#             #request.session['total_discount'] = request.session.get('total_discount', 0) + discountValue
+
+#             #request.session['total_discount'] = discountValue
+
+#             #discounted_total = total - (total * discount_percentage)
+#             #total -= discount.amount  # You may need to adjust this based on how your discount model is structured
+#         except ObjectDoesNotExist:
+#             # Handle the case where the discount code is not valid
+#             pass
+#     return total
+
+
+def apply_discount_to_product(request, product, total, discount_code, quantity):
+    if discount_code:
+        try:
+            # Retrieve the discount code from the database
+            discount = PromoCodes.objects.get(code=discount_code)
+
+            if discount.product.id == product.id:
+                # Calculate the discounted total
+                discountValue = round(
+                    (((product.price * quantity) / 100) * discount.discount_percentage),
+                    2,
+                )
+
+                # Retrieve the current total from the session
+                original_total_discount = Decimal(
+                    request.session.get("total_discount", "0")
+                )
+
+                # Update the session with the new discounted total
+                request.session["total_discount"] = str(
+                    original_total_discount + discountValue
+                )
+
+                # Calculate the updated total after applying the discount
+                discounted_total = total - discountValue
+
+                # If you need to use the original total elsewhere, you can keep it in the session
+                request.session["original_total"] = str(total)
+
+                # Return the discounted total
+                return discounted_total
+        except PromoCodes.DoesNotExist:
+            # Handle the case where the discount code is not valid
+            pass
+
+    # Return the original total if no discount is applied or an exception occurs
+    return total
--- bag/views.py
+++ bag/views.py
@@ -1,137 +1,172 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
+from django.shortcuts import HttpResponse, get_object_or_404, redirect, render, reverse
 
 from products.models import Product
 
 # Create your views here.
 
+
 def view_bag(request):
     """A view to return the bag page"""
 
-    return render(request, 'bag/bag.html')
+    return render(request, "bag/bag.html")
+
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of items to the shopping basket """
+    """Add a quantity of items to the shopping basket"""
 
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    quantity = int(request.POST.get("quantity"))
+    redirect_url = request.POST.get("redirect_url")
     size = None
     weight = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    if 'product_weight' in request.POST:
-        weight = request.POST['product_weight']
-    bag = request.session.get('bag', {})
+    if "product_size" in request.POST:
+        size = request.POST["product_size"]
+    if "product_weight" in request.POST:
+        weight = request.POST["product_weight"]
+    bag = request.session.get("bag", {})
 
     if weight:
         if item_id in list(bag.keys()):
-            if weight in bag[item_id]['items_by_weight'].keys():
-                bag[item_id]['items_by_weight'][weight] += quantity
-                messages.success(request, f'Updated weight {weight.upper()} {product.name} quantity to {bag[item_id]["items_by_weight"][weight]}')
+            if weight in bag[item_id]["items_by_weight"].keys():
+                bag[item_id]["items_by_weight"][weight] += quantity
+                messages.success(
+                    request,
+                    f'Updated weight {weight.upper()} {product.name} quantity to {bag[item_id]["items_by_weight"][weight]}',
+                )
             else:
-                bag[item_id]['items_by_weight'][weight] = quantity
-                messages.success(request, f'Added weight {weight.upper()} {product.name} to your bag')
+                bag[item_id]["items_by_weight"][weight] = quantity
+                messages.success(
+                    request, f"Added weight {weight.upper()} {product.name} to your bag"
+                )
         else:
-            bag[item_id] = {'items_by_weight': {weight: quantity}}
-            messages.success(request, f'Added weight {weight.upper()} {product.name} to your bag')
+            bag[item_id] = {"items_by_weight": {weight: quantity}}
+            messages.success(
+                request, f"Added weight {weight.upper()} {product.name} to your bag"
+            )
     elif size:
         if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
+            if size in bag[item_id]["items_by_size"].keys():
+                bag[item_id]["items_by_size"][size] += quantity
+                messages.success(
+                    request,
+                    f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}',
+                )
             else:
-                bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
+                bag[item_id]["items_by_size"][size] = quantity
+                messages.success(
+                    request, f"Added size {size.upper()} {product.name} to your bag"
+                )
         else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
+            bag[item_id] = {"items_by_size": {size: quantity}}
+            messages.success(
+                request, f"Added size {size.upper()} {product.name} to your bag"
+            )
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
-            messages.success(request, f'Updated {product.name}quantity to {bag[item_id]}')
+            messages.success(
+                request, f"Updated {product.name}quantity to {bag[item_id]}"
+            )
         else:
             bag[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your bag')
+            messages.success(request, f"Added {product.name} to your bag")
 
-    request.session['bag'] = bag
+    request.session["bag"] = bag
     return redirect(redirect_url)
 
+
 def adjust_bag(request, item_id):
-    """ Adjust the quantity of products to a specified amount """
+    """Adjust the quantity of products to a specified amount"""
     product = get_object_or_404(Product, pk=item_id)
-    quantity = int(request.POST.get('quantity'))
+    quantity = int(request.POST.get("quantity"))
 
     size = None
     weight = None
-    if 'product_size' in request.POST:
-        size = request.POST['product_size']
-    if 'product_weight' in request.POST:
-        size = request.POST['product_weight']
+    if "product_size" in request.POST:
+        size = request.POST["product_size"]
+    if "product_weight" in request.POST:
+        size = request.POST["product_weight"]
 
-    bag = request.session.get('bag', {})
+    bag = request.session.get("bag", {})
 
     if weight:
         if quantity > 0:
-            bag[item_id]['items_by_weight'][weight] = quantity
-            messages.success(request, f'Updated quantity to {quantity}')
+            bag[item_id]["items_by_weight"][weight] = quantity
+            messages.success(request, f"Updated quantity to {quantity}")
         else:
-            del bag[item_id]['items_by_weight'][weight]
-            if not bag[item_id]['items_by_weight']:
+            del bag[item_id]["items_by_weight"][weight]
+            if not bag[item_id]["items_by_weight"]:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {weight.upper()} {product.name} to your bag')
+            messages.success(
+                request, f"Removed size {weight.upper()} {product.name} to your bag"
+            )
     elif size:
         if quantity > 0:
-            bag[item_id]['items_by_size'][size] = quantity
-            messages.success(request, f'Updated quantity to {quantity}')
+            bag[item_id]["items_by_size"][size] = quantity
+            messages.success(request, f"Updated quantity to {quantity}")
         else:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
+            del bag[item_id]["items_by_size"][size]
+            if not bag[item_id]["items_by_size"]:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} to your bag')
+            messages.success(
+                request, f"Removed size {size.upper()} {product.name} to your bag"
+            )
     else:
         if quantity > 0:
             bag[item_id] = quantity
-            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
+            messages.success(
+                request, f"Updated {product.name} quantity to {bag[item_id]}"
+            )
         else:
             bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
+            messages.success(request, f"Removed {product.name} from your bag")
 
-    request.session['bag'] = bag
-    return redirect(reverse('view_bag'))
+    request.session["bag"] = bag
+    return redirect(reverse("view_bag"))
 
 
 def remove_from_bag(request, item_id):
-    """ Adjust the quantity of products to a specified amount """
+    """Adjust the quantity of products to a specified amount"""
 
     try:
         product = get_object_or_404(Product, pk=item_id)
         size = None
         weight = None
-        if 'product_size' in request.POST:
-            size = request.POST['product_size']
-        if 'product_weight' in request.POST:
-            weight = request.POST['product_weight']
-        bag = request.session.get('bag', {})
+        if "product_size" in request.POST:
+            size = request.POST["product_size"]
+        if "product_weight" in request.POST:
+            weight = request.POST["product_weight"]
+        bag = request.session.get("bag", {})
 
         if size:
-            del bag[item_id]['items_by_size'][size]
-            if not bag[item_id]['items_by_size']:
+            del bag[item_id]["items_by_size"][size]
+            if not bag[item_id]["items_by_size"]:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(
+                request, f"Removed size {size.upper()} {product.name} from your bag"
+            )
         elif weight:
-            del bag[item_id]['items_by_weight'][weight]
-            if not bag[item_id]['items_by_weight']:
+            del bag[item_id]["items_by_weight"][weight]
+            if not bag[item_id]["items_by_weight"]:
                 bag.pop(item_id)
 
         else:
             bag.pop(item_id)
-            messages.success(request, f'Removed {product.name} from your bag')
+            messages.success(request, f"Removed {product.name} from your bag")
 
-
-
-        request.session['bag'] = bag
+        request.session["bag"] = bag
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
+        messages.error(request, f"Error removing item: {e}")
         return HttpResponse(status=500)
+
+
+def apply_discount(request):
+    if request.method == "POST":
+        discount_code = request.POST.get(
+            "promoCode", None
+        )
+        request.session["discount_code"] = discount_code
+    return HttpResponse(status=200)
+
