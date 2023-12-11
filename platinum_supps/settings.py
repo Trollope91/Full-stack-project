01e167ab717bdfa16ac28a8275bca39d8f99e220
Trollope91 <ljtdev91@gmail.com>
2023-12-11 20:29:35 +0000

    Update email address in settings

--- platinum_supps/settings.py
+++ platinum_supps/settings.py
@@ -102,13 +102,11 @@ SITE_ID = 1
 
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = "None"
+ACCOUNT_EMAIL_VERIFICATION = "Mandatory"
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = "/accounts/login/"
 LOGIN_REDIRECT_URL = "/"
-
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 WSGI_APPLICATION = "platinum_supps.wsgi.application"
 
@@ -212,37 +210,10 @@ STRIPE_PUBLIC_KEY = os.getenv("STRIPE_PUBLIC_KEY", "")
 STRIPE_SECRET_KEY = os.getenv("STRIPE_SECRET_KEY", "")
 STRIPE_WH_SECRET = os.getenv("STRIPE_WH_SECRET", "")
 
-
-# if "Debug" in os.environ:
-#     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-#     DEFAULT_FROM_EMAIL = "platinumsupps@example.com"
-
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# DEFAULT_FROM_EMAIL = 'django'
-# EMAIL_HOST_USER = 'actualvenusaur@gmail.com'
-# EMAIL_HOST_PASSWORD = 'hpbudutamxsobmih'
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-DEFAULT_FROM_EMAIL = 'django'
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASS", "")
-
-
-
-
-
-
-
-# #else:
-#     EMAIL_USE_TLS = True
-#     EMAIL_PORT = 587
-#     EMAIL_HOST = "smtp.sendgrid.net"
-#     EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-#     EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
-#     DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
